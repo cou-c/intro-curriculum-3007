e8b09b8144e1f0544b1af379ee183346a6af8b39
cou <courange.c@gmail.com>
2020-04-06 02:30:37 +0900

    N課題

--- index.js
+++ index.js
@@ -1,4 +1,13 @@
 'use strict';
 
+/**
+ * 整数が17で割り切れるかどうか判定する
+ * @param {int} x
+ * @return {boolean} 
+ */
+function isMultipleOfSeventeen(x){
+  return x%17 === 0;
+}
 module.exports = {
+  isMultipleOfSeventeen
 }
